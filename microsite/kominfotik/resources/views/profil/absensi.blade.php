b76dc9318e6645732f9f3b6585e57e384fc91e47
hideky <afganhalimi@gmail.com>
2023-09-28 11:09:55 +0700

    Penambahan grafik rekapitulasi absensi tahunan di menu profil tiap tenaga terampil

--- microsite/kominfotik/resources/views/profil/absensi.blade.php
+++ microsite/kominfotik/resources/views/profil/absensi.blade.php
@@ -1,109 +1,177 @@
 <hr>
-<div class="row in collapse show" id="kegiatan">
-<h6 class="mb-5">Laporan Absensi Bulan Lalu</h6>
-<div class="col-12">
-    <div class="card z-index-2  ">
-    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-        <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
-        <div class="chart">
-            <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
+<div class="row in collapse show" id="absensi">
+    <h6 class="mb-5">Laporan Absensi Awal Tahun s/d Sekarang</h6>
+    <div class="col-12">
+        <div class="card z-index-2  ">
+        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div id="grafik_absen_2" style="width: 100%; height: 500px;"></div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div id="grafik_absen_1" style="width: 100%; height: 500px;"></div>
+                </div>
+            </div>
         </div>
+            <div class="card-body">
+                <h6 class="mb-2 "> Rekapitulasi Keterlambatan: </h6>
+                <p class="text-sm mb-0"> Total Telat: <span class="font-weight-bolder">{{ $absensi['total_telat'] }} jam</span></p>
+                <p class="text-sm "> Total Pulang Cepat: <span class="font-weight-bolder">{{ $absensi['total_pulang_cepat'] }} jam</span></p>
+            </div>
         </div>
-    </div>
-    <div class="card-body">
-        <h6 class="mb-0 "> Daily Sales </h6>
-        <p class="text-sm "> (<span class="font-weight-bolder">+15%</span>) increase in today sales. </p>
-        <hr class="dark horizontal">
-        <div class="d-flex ">
-        <i class="material-icons text-sm my-auto me-1">schedule</i>
-        <p class="mb-0 text-sm"> updated 4 min ago </p>
-        </div>
-    </div>
     </div>
 </div>
-</div>
-<script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
 <script>
-    var ctx2 = document.getElementById("chart-line").getContext("2d");
-
-    new Chart(ctx2, {
-      type: "line",
-      data: {
-        labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [{
-          label: "Mobile apps",
-          tension: 0,
-          borderWidth: 0,
-          pointRadius: 5,
-          pointBackgroundColor: "rgba(255, 255, 255, .8)",
-          pointBorderColor: "transparent",
-          borderColor: "rgba(255, 255, 255, .8)",
-          borderColor: "rgba(255, 255, 255, .8)",
-          borderWidth: 4,
-          backgroundColor: "transparent",
-          fill: true,
-          data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-          maxBarThickness: 6
-
-        }],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          }
+let total_cuti = {{ $absensi['total_cuti'] }};
+let total_izin = {{ $absensi['total_izin'] }};
+let total_alpha = {{ $absensi['total_alpha'] }};
+let total_sakit = {{ $absensi['total_sakit'] }};
+let total_dinas_luar_penuh = {{ $absensi['total_dinas_luar_penuh'] }};
+let total_dinas_luar_awal = {{ $absensi['total_dinas_luar_awal'] }};
+let total_dinas_luar_akhir = {{ $absensi['total_dinas_luar_akhir'] }};
+Highcharts.chart('grafik_absen_1', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: '',
+        align: 'left'
+    },
+    xAxis: {
+        categories: ['Cuti', 'Izin', 'Alpha', 'Sakit', 'Dinas Luar Penuh', 'Dinas Luar Awal', 'Dinas Luar Akhir'],
+        title: {
+            text: null
         },
-        interaction: {
-          intersect: false,
-          mode: 'index',
+        gridLineWidth: 1,
+        lineWidth: 0
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Jumlah dalam hari',
+            align: 'high'
         },
-        scales: {
-          y: {
-            grid: {
-              drawBorder: false,
-              display: true,
-              drawOnChartArea: true,
-              drawTicks: false,
-              borderDash: [5, 5],
-              color: 'rgba(255, 255, 255, .2)'
+        labels: {
+            overflow: 'justify'
+        },
+        gridLineWidth: 0
+    },
+    tooltip: {
+        valueSuffix: ' hari'
+    },
+    plotOptions: {
+        bar: {
+            borderRadius: '50%',
+            dataLabels: {
+                enabled: true
             },
-            ticks: {
-              display: true,
-              color: '#f8f9fa',
-              padding: 10,
-              font: {
-                size: 14,
-                weight: 300,
-                family: "Roboto",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
-          x: {
-            grid: {
-              drawBorder: false,
-              display: false,
-              drawOnChartArea: false,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              color: '#f8f9fa',
-              padding: 10,
-              font: {
-                size: 14,
-                weight: 300,
-                family: "Roboto",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
+            groupPadding: 0.1
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Jumlah',
+        data: [total_cuti, total_izin, total_alpha, total_sakit, total_dinas_luar_penuh, total_dinas_luar_awal, total_dinas_luar_akhir]
+    }]
+});
+</script>
+<script>
+var total_telat_bulanan = [
+    {{ $absensi['total_telat_bulanan'][1] }},
+    {{ $absensi['total_telat_bulanan'][2] }},
+    {{ $absensi['total_telat_bulanan'][3] }},
+    {{ $absensi['total_telat_bulanan'][4] }},
+    {{ $absensi['total_telat_bulanan'][5] }},
+    {{ $absensi['total_telat_bulanan'][6] }},
+    {{ $absensi['total_telat_bulanan'][7] }},
+    {{ $absensi['total_telat_bulanan'][8] }},
+    {{ $absensi['total_telat_bulanan'][9] }},
+    {{ $absensi['total_telat_bulanan'][10] }},
+    {{ $absensi['total_telat_bulanan'][11] }},
+    {{ $absensi['total_telat_bulanan'][12] }},
+];
+
+var total_pulang_cepat_bulanan = [
+    {{ $absensi['total_pulang_cepat_bulanan'][1] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][2] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][3] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][4] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][5] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][6] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][7] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][8] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][9] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][10] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][11] }},
+    {{ $absensi['total_pulang_cepat_bulanan'][12] }},
+];
+Highcharts.chart('grafik_absen_2', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: '',
+        align: 'left'
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        title: {
+            text: null
         },
-      },
-    });
+        gridLineWidth: 1,
+        lineWidth: 0
+    },
+    yAxis: {
+        title: {
+            text: 'Jam (hh:mm:ss)'
+        },
+        labels: {
+            formatter: function () {
+                var time = this.value;
+                var hours = parseInt(time/3600000);
+                var minutes = parseInt((parseInt(time%3600000))/60000);
+                var seconds = parseInt(parseInt((parseInt(time%3600000))%60000))/1000;
+                return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            }
+        }
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    tooltip: {
+        formatter: function () {
+            var time = this.y;
+            var hours = parseInt(time/3600000);
+            var minutes = parseInt((parseInt(time%3600000))/60000);
+            var seconds = parseInt(parseInt((parseInt(time%3600000))%60000))/1000;
+            return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} Jam`;
+        }
+    },
+    series: [{
+        name: 'Jam Telat',
+        data: total_telat_bulanan
+    }, {
+        name: 'Jam Pulang Cepat',
+        data: total_pulang_cepat_bulanan
+    }]
+});
+
 </script>
