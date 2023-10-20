1b6c3c511aee5a8aebfe1a0124115fe2f72a1d74
Afgan <afganhalimi@gmail.com>
2023-10-20 13:28:56 +0700

    Penambahan function untuk mengecek kode layanan berdasarkan seksi di microsite BATIK

--- microsite/batik/app/Http/Controllers/LayananController.php
+++ microsite/batik/app/Http/Controllers/LayananController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LayananAstik;
+use App\Models\LayananId;
+use App\Models\LayananKip;
 use App\Traits\ApiBarat;
+use Illuminate\Http\Request;
 
 class LayananController extends Controller
 {
@@ -20,9 +24,28 @@ class LayananController extends Controller
         ]);
     }
 
-    public function cek_status_detail() {
+    public function cek_status_detail(Request $request) {
+        $footer_secaw_infografis = $this->get_latest_infografis();
+        $berita = $this->get_latest_berita();
+        $layanan = LayananId::where('kode_layanan', $request->get('kode_layanan'))->first();
+        if($layanan == null) {
+            $layanan = LayananKip::where('kode_layanan', $request->get('kode_layanan'))->first();
+            $layanan_detail = "KIP";
+            if($layanan == null) {
+                $layanan = LayananAstik::where('kode_layanan', $request->get('kode_layanan'))->first();
+                $layanan_detail = "ASTIK";
+            }
+        }
+        else {
+            $layanan_detail = "ID";
+        }
         return view("main", [
-            'detail_page' => "Detail Layanan",
+            'page' => "Layanan",
+            'subpage' => "Cek Status Layanan",
+            'footer_secaw_infografis' => $footer_secaw_infografis,
+            'berita' => $berita,
+            'layanan' => $layanan,
+            'layanan_detail' => $layanan_detail,
         ]);
     }
 }
